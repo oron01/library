2a02184ff6982d5e7761abb4414b8befbf50d05a
oron01 <136211946+oron01@users.noreply.github.com>
2023-11-18 08:00:54 +0200

    changed readinput to custom radio, change page count to numbers

--- code.js
+++ code.js
@@ -4,6 +4,21 @@ let submitButton = document.querySelector(".submit")
 let dialog = document.querySelector("dialog")
 let overlay = document.querySelector(".overlay")
 
+let getBookInfo = () => {
+    let title = document.querySelector('#title').value
+    let pages = document.querySelector('#pages').value
+    let author = document.querySelector('#author').value
+    let read = document.querySelector('#read').value
+}
+
+function Book(title,pages,author,read) {
+    self.title = title
+    self.pages = pages
+    self.author = author
+    self.read = read
+    self.text = `${self.title} by ${self.author}, ${self.pages}, ${self.read}`
+}
+
 submitButton.addEventListener('click',(event) => {
     dialog.close()
     overlay.style.opacity = "0"
